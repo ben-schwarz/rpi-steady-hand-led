af546f517229a6188847c1d12ae318d36410c2e0
ben-schwarz <ben@k-schwarz.com>
2016-10-22 16:42:42 -0400

    Modifyed the code to include a seven segment display.

--- RpiSteadyHands.py
+++ RpiSteadyHands.py
@@ -2,20 +2,29 @@
 # Steady hands game
 import RPi.GPIO as GPIO
 import time
-# use BCM GPIO numbering ­ use anything else and you are an idiot!
+from Adafruit_LED_Backpack import SevenSegment
+
+
+# set up and start the display.
+display = SevenSegment.SevenSegment()
+display.begin()
+
+
+# use BCM GPIO numbering - use anything else and you are an idiot!
 GPIO.setmode(GPIO.BCM)
 # set up GPIO input pins
 # (pull_up_down be PUD_OFF, PUD_UP or PUD_DOWN, default PUD_OFF)
 GPIO.setup(4, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 # GPIO 0 & 1 have hardware pull ups fitted in the Pi so don't enable them
-GPIO.setup(0, GPIO.IN, pull_up_down=GPIO.PUD_OFF)
-GPIO.setup(1, GPIO.IN, pull_up_down=GPIO.PUD_OFF)
-print("Hi from Python :­ Steady Hands game")
+# 0 & 1 will be used for seven segment display. So, turn hardware pull ups on.
+GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(22, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+print("Hi from Python :- Steady Hands game")
 delay = range(0, 5000)
 dum = 0
 start_rest = 4
-end_rest = 0
-wire = 1
+end_rest = 22
+wire = 17
 while True:
 # wait until the wand is at the start
 	print("Move the loop to the start rest")
@@ -35,7 +44,10 @@ while True:
 		if GPIO.input(wire) == 0:
 			penalty = penalty + 1
 			print("Penalties total", penalty, " points")
+			display.clear()
+			display.print_float(penalty, decimal_digits=0, justify_right=False)
+			display.write_display()
 			time.sleep(0.07)
-	score = time.clock() ­ run_time + (penalty * 0.07)
+                score = time.clock() - run_time + (penalty * 0.07)
 	print("The run time was", score, "seconds with", penalty, "Penalty points")
 # finished a run so start again
